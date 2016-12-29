23b9a58c3868db7ed6acaa8fef53d7523509f5b4
zentoku <zentoku@sea.plala.or.jp>
2016-12-29 15:04:14 +0900

    微修正

--- Assets/Scripts/Models/Utilities/File.cs
+++ Assets/Scripts/Models/Utilities/File.cs
@@ -5,7 +5,7 @@ public static class File
 {
     public static void Write(string filePath, string contents)
     {
-        using (var fileStream = new FileStream(filePath, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write, System.IO.FileShare.None))
+        using (var fileStream = new FileStream(filePath, System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None))
         {
             using (var writer = new StreamWriter(fileStream))
             {
